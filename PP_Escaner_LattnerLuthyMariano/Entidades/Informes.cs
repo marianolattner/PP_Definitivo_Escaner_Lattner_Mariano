042a7113ed18dfc32912488c990543eeae31e35c
marianolattner <124000090+marianolattner@users.noreply.github.com>
2024-05-22 15:10:47 -0300

    Add files via upload

---
+++ PP_Escaner_LattnerLuthyMariano/Entidades/Informes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Entidades.Documento;
+
+namespace Entidades
+{
+    public static class Informes
+    {
+
+        // muestra la cantidad de documentos que hay por estado, la cantidad de paginas o de superficie y todos los datos del ToString del documento
+        private static void MostrarDocumentosPorEstado(Escaner e, Paso estado, out int extension, out int cantidad, out string resumen)
+        {
+            extension = 0;
+            cantidad = 0;
+            resumen = "";
+
+            foreach (Documento doc in e.ListaDocumentos)
+            {
+                if (doc.Estado == estado)
+                {
+                    if (doc is Libro)
+                    {
+                        Libro libro = (Libro)doc;
+                        extension += libro.NumPaginas;
+                    }
+                    else
+                    {
+                        if (doc is Mapa mapa)
+                        {
+                            extension += mapa.Superficie;
+                        }
+                    }
+                    cantidad++;
+                    resumen += doc.ToString();
+                }
+            }
+
+        }
+
+        public static void MostrarDistribuidos(Escaner e, out int extension, out int cantidad, out string resumen)
+        {
+            MostrarDocumentosPorEstado(e, Documento.Paso.Distribuido, out  extension, out  cantidad, out  resumen);
+        }
+
+        public static void MostrarEnEscaner(Escaner e, out int extension, out int cantidad, out string resumen)
+        {
+            MostrarDocumentosPorEstado(e, Documento.Paso.EnEscaner, out extension, out cantidad, out resumen);
+        }
+
+        public static void MostrarEnRevision(Escaner e, out int extension, out int cantidad, out string resumen)
+        {
+            MostrarDocumentosPorEstado(e, Documento.Paso.EnRevision, out extension, out cantidad, out resumen);
+        }
+
+        public static void MostrarTerminados(Escaner e, out int extension, out int cantidad, out string resumen)
+        {
+            MostrarDocumentosPorEstado(e, Documento.Paso.Terminado, out extension, out cantidad, out resumen);
+        }
+    }
+}
